df5d857c68aeac407656fed3c3ff176ecbc3e051
fuzakenshine <fuzakenshine@gmail.com>
2025-03-26 17:34:37 +0800

    Add get

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// Import routes
+const examGroupRoute = require('./routes/exam-group');
+const examsRoute = require('./routes/addget');  // New exams route
+
+// Use the routes
+app.use('/api', examGroupRoute);  // Existing examGroup route
+app.use('/api', examsRoute);      // New exams route
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
